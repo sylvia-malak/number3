6112c9b805f2263e5b18e1eab404875736db21c8
sylvia-malak <Sylviamalak49@gmail.com>
2024-11-15 15:14:42 +0200

    Update nameChecker.js

--- starter_project/src/client/js/nameChecker.js
+++ starter_project/src/client/js/nameChecker.js
@@ -1,19 +1,17 @@
-function checkForName(inputText) {
+function checkForName(inputText, validNames) {
     console.log("::: Running checkForName :::", inputText);
-    let names = [
-        "Picard",
-        "Janeway",
-        "Kirk",
-        "Archer",
-        "Georgiou"
-    ];
-
-    if(names.includes(inputText)) {
-        alert("Welcome, Captain!");
+    
+    const normalizedInput = inputText.trim().toLowerCase();
+    const normalizedNames = validNames.map(name => name.toLowerCase());
+    
+    if (normalizedNames.includes(normalizedInput)) {
+      return "Hello!"; // Return a success message
+    } else {
+      return "Enter a valid name"; // Return an error message
     }
-    else {
-        alert("Enter a valid captain name");
-    }
-}
+  }
+  
+  const names = ["Picard", "Janeway", "Kirk", "Archer", "Georgiou"];
+  export { checkForName };
+  
 
-export { checkForName };
